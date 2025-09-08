4e10197160b53f3f8590ac0de1ef027e9642305a
ssktyOar <ssktyOar@users.noreply.github.com>
2025-09-09 02:51:27 +1200

    Final test has been passed, all bugs has been found

--- bowling_game.py
+++ bowling_game.py
@@ -24,40 +24,49 @@ class BowlingGame:
     def score(self):
         """Calculate the score for the current game."""
         score = 0
+        roll_index = 0
         frame_index = 0
         print(self.rolls)
         print()
         while True:
             print()
             print("Score: " + str(score))
-            if not (frame_index < len(self.rolls)):
+            if not (roll_index < len(self.rolls)):
                 break
-            print("Frame index: " + str(frame_index))
-            if self._is_strike(frame_index):
-                if not (frame_index + 2 < len(self.rolls)):
+            print("Frame index: " + str(roll_index))
+            if frame_index == 2:
+                frame_index = 0
+            if self._is_strike(roll_index): #----- Strike
+                if not (roll_index + 2 < len(self.rolls)):
                     break
-                # Strike
-                score += 10 + self._strike_bonus(frame_index)
-                frame_index += 1
-            elif self._is_spare(frame_index):
-                if not (frame_index + 2 < len(self.rolls)):
-                    print("Final frame index: " + str(frame_index))
+
+                print("Strike!")
+                score += 10 + self._strike_bonus(roll_index)
+                roll_index += 1
+                frame_index = 0
+            elif self._is_spare(roll_index) and frame_index == 0: #---- Spare
+
+                if not (roll_index + 2 < len(self.rolls)):
+                    print("Final frame index: " + str(roll_index))
+                    break              
+                print("Spare!")
+                score += 10 + self._spare_bonus(roll_index)
+                roll_index += 2
+                
+                if (roll_index + 1) == len(self.rolls):
                     break
-                # Spare
-                score += 10 + self._spare_bonus(frame_index)
-                frame_index += 2
-                if (frame_index + 1) == len(self.rolls):
-                    break
-            else:
-                # Open frame
-                score += self.rolls[frame_index]
+                frame_index = 0
+            else: # ----------------------------- Open frame
+                score += self.rolls[roll_index]
+                print(self.rolls[roll_index])
+                roll_index += 1
                 frame_index += 1
         print()
-        print("Final frame index: " + str(frame_index))
+        print("Final frame index: " + str(roll_index))
         print("Rolls: " + str(len(self.rolls)))
         return score
 
-    def _is_strike(self, frame_index):
+    def _is_strike(self, roll_index):
         """
         Check if the roll at frame_index is a strike.
 
@@ -67,9 +76,9 @@ class BowlingGame:
         Returns:
             True if the roll is a strike, False otherwise
         """
-        return frame_index < len(self.rolls) and self.rolls[frame_index] == 10
+        return roll_index < len(self.rolls) and self.rolls[roll_index] == 10
 
-    def _is_spare(self, frame_index):
+    def _is_spare(self, roll_index):
         """
         Check if the rolls at frame_index and frame_index + 1 form a spare.
 
@@ -79,9 +88,9 @@ class BowlingGame:
         Returns:
             True if the rolls form a spare, False otherwise
         """
-        return frame_index + 1 < len(self.rolls) and self.rolls[frame_index] + self.rolls[frame_index + 1] == 10
+        return roll_index + 1 < len(self.rolls) and self.rolls[roll_index] + self.rolls[roll_index + 1] == 10
 
-    def _strike_bonus(self, frame_index):
+    def _strike_bonus(self, roll_index):
         """
         Calculate the bonus for a strike.
 
@@ -91,7 +100,7 @@ class BowlingGame:
         Returns:
             The value of the next two rolls after the strike
         """
-        return (self.rolls[frame_index + 1] + self.rolls[frame_index + 2])
+        return (self.rolls[roll_index + 1] + self.rolls[roll_index + 2])
 
     def _spare_bonus(self, frame_index):
         """
--- test_pytest.py
+++ test_pytest.py
@@ -4,14 +4,20 @@ from bowling_game import BowlingGame
 rollsNormal = [10, 3, 6, 5, 5, 8, 1, 10, 10, 10, 9, 0, 7, 3, 10, 10, 8]
 rollsPerfect = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
 rollsSpares = [5 for i in range(21)]
+rollsGutter = [0 for i in range(20)]
+rollsRegular = [3, 4, 2, 5, 1, 6, 4, 2, 8, 1, 7, 1, 5, 3, 2, 3, 4, 3, 2, 6]
+
 @pytest.mark.parametrize("_count_numbers,expected,rolls", [
     (3, 28, rollsNormal),
     (7, 55, rollsNormal),
     (12, 142, rollsNormal),
     (17, 190, rollsNormal),
     (12, 300, rollsPerfect),
-    (21, 150, rollsSpares)
+    (21, 150, rollsSpares),
+    (20, 0, rollsGutter),
+    (20, 72, rollsRegular)
     ])
+
 def testGame(_count_numbers: int, expected: int, rolls: list):
 
     game = BowlingGame()
@@ -20,25 +26,3 @@ def testGame(_count_numbers: int, expected: int, rolls: list):
         game.roll(rolls[i])
 
     assert game.score() == expected
-
-# Testing
-
-# Frames and scoring:
-# Frame 1: 10 (Strike)         | 10 + 3 + 6 = 19
-# Frame 2: 3, 6                | 9
-# Expected: 28
-'''
-def test_1():
-    assert testGame(3) == 28
-'''
-
-#test
-
-'''
-    Frame 3: 5, 5 (Spare)        | 10 + 8 = 18
-    Frame 4: 8, 1                | 9
-'''
-'''
-def test_2():
-    assert testGame(3) == 28
-'''
