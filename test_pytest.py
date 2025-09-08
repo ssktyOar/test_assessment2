4aa0c2649c83500ec0ebbfeaecb5965a9d110b27
ssktyOar <ssktyOar@users.noreply.github.com>
2025-09-08 21:53:33 +1200

    Made main test function

--- test_pytest.py
+++ test_pytest.py
@@ -1,2 +1,14 @@
 import pytest
+from bowling_game import BowlingGame
+
+rolls = [10, 3, 6, 5, 5, 8, 10, 10, 10, 9, 0, 7, 3, 10, 10, 8]
+
+def test_game(expectedScore, count):
+    global rolls
+
+    game = BowlingGame()
+
+    for i in range(count):
+        game.roll(rolls[i])
+    assert expectedScore == game.score()
 
